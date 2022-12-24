5787d7005da55bcaa3d2a8fd820e1c4aa5628d12
Ghaffar Daher <ghaffardaher92@gmail.com>
2022-12-25 00:17:49 +0300

    after تخبيصة dark nova

--- resources/views/word_category.blade.php
+++ resources/views/word_category.blade.php
@@ -65,24 +65,26 @@
     <section class="dcare__blog__area section-padding--lg bg--white">
         <div class="container">
             <div class="row blog-page">
+
+            @foreach($categories as $category)
+        
                 <!-- Start Single Blog -->
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="blog__2">
                         <div class="blog__thumb">
                             <a href="blog-details.html">
-                                <img src="{{asset('images/category/home.png')}}" height="304" width="370"
+                                <img src="{{asset($category->url)}}" height="304" width="370"
                                      alt="blog images">
                             </a>
                         </div>
                         <div class="blog__inner">
                             <div class="blog__hover__inner">
-                                <h2><a href="blog-details.html">House Words</a></h2>
+                                <h2><a href="blog-details.html">{{$category->title}}</a></h2>
                                 <div class="bl__meta">
-                                    <p>Children Blog : Post By Ariana / 10th November, 2017</p>
+                                    <p>{{$category->created_at}}</p>
                                 </div>
                                 <div class="bl__details">
-                                    <p>Lorem ipsum dolor sit amet, consect adsicinge elit, sed do eiusmod tempor
-                                        incidi.</p>
+                                    <p>{{$category->description}}</p>
                                 </div>
                                 <div class="blog__btn">
                                     <a class="bl__btn" href="blog-details.html">Start Learning</a>
@@ -93,92 +95,8 @@
                     </div>
                 </div>
                 <!-- End Single Blog -->
-                <!-- Start Single Blog -->
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="blog__2">
-                        <div class="blog__thumb">
-                            <a href="blog-details.html">
-                                <img src="{{asset('images/category/family.png')}}" height="304" width="370"
-                                     alt="blog images">
-                            </a>
-                        </div>
-                        <div class="blog__inner">
-                            <div class="blog__hover__inner">
-                                <h2><a href="blog-details.html">Family Words</a></h2>
-                                <div class="bl__meta">
-                                    <p>Children Blog : Post By Ariana / 10th November, 2017</p>
-                                </div>
-                                <div class="bl__details">
-                                    <p>Lorem ipsum dolor sit amet, consect adsicinge elit, sed do eiusmod tempor
-                                        incidi.</p>
-                                </div>
-                                <div class="blog__btn">
-                                    <a class="bl__btn" href="blog-details.html">Read More</a>
-                                    <a class="bl__share__btn" href="#"><i class="fa fa-share-alt"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Blog -->
-                <!-- Start Single Blog -->
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="blog__2">
-                        <div class="blog__thumb">
-                            <a href="blog-details.html">
-                                <img src="{{asset('images/category/january.png')}}" height="304" width="370"
-                                     alt="blog images">
-                            </a>
-                        </div>
-                        <div class="blog__inner">
-                            <div class="blog__hover__inner">
-                                <h2><a href="blog-details.html">Date Words</a></h2>
-                                <div class="bl__meta">
-                                    <p>Children Blog : Post By Ariana / 10th November, 2017</p>
-                                </div>
-                                <div class="bl__details">
-                                    <p>Lorem ipsum dolor sit amet, consect adsicinge elit, sed do eiusmod tempor
-                                        incidi.</p>
-                                </div>
-                                <div class="blog__btn">
-                                    <a class="bl__btn" href="blog-details.html">Read More</a>
-                                    <a class="bl__share__btn" href="#"><i class="fa fa-share-alt"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Blog -->
-                @for($index = 0 ; $index < 10 ; $index++)
-                <!-- Start Single Blog -->
-                <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="blog__2">
-                        <div class="blog__thumb">
-                            <a href="blog-details.html">
-                                <img src="{{asset('images/category/number.png')}}" height="304" width="370"
-                                     alt="blog images">
-                            </a>
-                        </div>
-                        <div class="blog__inner">
-                            <div class="blog__hover__inner">
-                                <h2><a href="blog-details.html">Numbers Words</a></h2>
-                                <div class="bl__meta">
-                                    <p>Children Blog : Post By Ariana / 10th November, 2017</p>
-                                </div>
-                                <div class="bl__details">
-                                    <p>Lorem ipsum dolor sit amet, consect adsicinge elit, sed do eiusmod tempor
-                                        incidi.</p>
-                                </div>
-                                <div class="blog__btn">
-                                    <a class="bl__btn" href="blog-details.html">Read More</a>
-                                    <a class="bl__share__btn" href="#"><i class="fa fa-share-alt"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Blog -->
-                @endfor
+                
+            @endforeach
             </div>
             <div class="row">
                 <div class="col-lg-12">
