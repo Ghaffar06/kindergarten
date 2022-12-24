2cfeee7aa8ab37d9b73ec5cc89b1c8d9e7b99b0b
Mohamad-AlAswad <creative12.syr@gmail.com>
2022-12-24 21:38:22 +0300

    finishing category controller, with view example.

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,26 +7,44 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('test', ['categories' => Category::all()]);
+        $categories = [];
+        if ($title = $request->get('search')) {
+            $categories = Category::where('title', 'LIKE', '%' . $title . '%')
+                ->get();
+        } else {
+            $categories = Category::all();
+        }
+        return view('test', ['categories' => $categories]);
     }
-    public function create(Request $request){
+
+    public function create(Request $request)
+    {
         $request->validate([
             'image' => 'required|image|mimes:png,jpg,jpeg|max:2048',
-            'description'=>'required',
-            'title'=>'required',
+            'description' => 'required',
+            'title' => 'required',
         ]);
-        $category =  new Category(request()->all());
-        $category->url = $category->title.time().'.'.$request->image->extension();
+        $category = new Category(request()->all());
+        $category->url = $category->title . time() . '.' . $request->image->extension();
         $request->image->move(public_path('images/data/category'), $category->url);
-        $category->url = 'images/data/category/'.$category->url;
+        $category->url = 'images/data/category/' . $category->url;
         $category->save();
         return back()->with('success', 'category added successfully');
     }
-    public function delete($id){
-        if (Category::where('id', '=' , $id)->delete())
+
+    public function delete($id)
+    {
+        if (Category::where('id', '=', $id)->delete())
             return back()->with('success', 'deleted successfully');
         return back()->with('error', 'failed!');
+    }
+
+    public function autocompleteSearch(Request $request)
+    {
+        $query = $request->get('query');
+        $filterResult = Category::where('title', 'LIKE', '%' . $query . '%')->get('title');
+        return response()->json($filterResult);
     }
 }
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"/>
     <style>
         .container {
             max-width: 600px;
@@ -15,79 +15,89 @@
     </style>
 </head>
 <body>
-    <p1>Category Section!</p1>
-    <br>
-    <br>
+<p1>Category Section!</p1>
+<br>
+<br>
 
-    <div>
-        <p3>add new category:</p3>
-        @error('image')
-        <br>
-        <span class="invalid-feedback" role="alert">
+<div>
+    <p3>add new category:</p3>
+    @error('image')
+    <br>
+    <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
-        @enderror
-        @error('description')
-        <br>
-        <span class="invalid-feedback" role="alert">
+    @enderror
+    @error('description')
+    <br>
+    <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
-        @enderror
-        @error('title')
-        <br>
-        <span class="invalid-feedback" role="alert">
+    @enderror
+    @error('title')
+    <br>
+    <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
-        @enderror
+    @enderror
 
 
-        <form action="{{route('category.add')}}" method="post" enctype="multipart/form-data">
-            @csrf
-            <label for="title">title: </label>
-            <input type="text" name="title">
-            <br>
-            <label for="description">description: </label>
-            <input type="text" name="description">
-            <br>
-            <label for="image">image: </label>
-            <input type="file" name="image">
-            <br>
-            <button type="submit">Submit</button>
-            <br>
-        </form>
-    </div>
-    <br>
-    <div>
-        <p3>all categories:</p3>
-        @foreach($categories as $category)
-            <div>
-                {{$category}}
-                <br>
-                <img src="{{asset($category->url)}}" alt="photo" width="100px" height="100px">
-                <br>
-            </div>
-        @endforeach
-    </div>
-    <p3>operations:</p3>
-    <br>
-    <br>
-    <div>
-        id of category to delete:
-        <input type="text" id = 'id1'>
-        <button onclick="deleteCategory()">delete!</button>
+    <form action="{{route('category.add')}}" method="post" enctype="multipart/form-data">
+        @csrf
+        <label for="title">title: </label>
+        <input type="text" name="title">
         <br>
-    </div>
-    <br>
-    <div>
-        search by title or description of a category:
-        <div class="container mt-5">
-            <div classs="form-group">
-                <input type="text" id="search" name="search" placeholder="Search" class="form-control" />
-            </div>
+        <label for="description">description: </label>
+        <input type="text" name="description">
+        <br>
+        <label for="image">image: </label>
+        <input type="file" name="image">
+        <br>
+        <button type="submit">Submit</button>
+        <br>
+    </form>
+</div>
+<br>
+<div>
+    <p3>all categories:</p3>
+    @foreach($categories as $category)
+        <div>
+            {{$category}}
+            <br>
+            <img src="{{asset($category->url)}}" alt="photo" width="100px" height="100px">
+            <br>
         </div>
-
-    </div>
+    @endforeach
+</div>
+<p3>operations:</p3>
+<br>
+<br>
+<div>
+    id of category to delete:
+    <input type="text" id='id1'>
+    <button onclick="deleteCategory()">delete!</button>
     <br>
+</div>
+<br>
+<div>
+    id of category to display:
+    <input type="text" id='id11'>
+    <button onclick="openCategory()">open</button>
+    <a hidden="true" href="" id="href-words" target="_blank"></a>
+    <br>
+</div>
+<br>
+
+<div>
+    search by title or description of a category:
+    <div class="container mt-5">
+        <div classs="form-group">
+            <input type="text" id="search" name="search" placeholder="Search" class="form-control"/>
+        </div>
+        <button onclick="search()">Search!</button>
+    </div>
+
+</div>
+<br>
 
 </body>
 <script>
@@ -97,20 +107,33 @@
         url = url.replace(':id', id);
         window.location.replace(url);
     }
+
+    function openCategory() {
+        let id = document.getElementById('id11').value;
+        let url = "{{route('category.delete' , ['id'=>':id'])}}";
+        url = url.replace(':id', id);
+        document.getElementById('href-words').setAttribute('href', url);
+        document.getElementById('href-words').click();
+    }
+
+    function search() {
+        let title = document.getElementById('search').value;
+        window.location.replace("{{route('category.index')}}" + "?search=" + title);
+    }
 </script>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js">
 </script>
 <script type="text/javascript">
-    var route = "{{ url('autocomplete-search') }}";
+    var route = "{{ route('category.autocomplete-search') }}";
     $('#search').typeahead({
         source: function (query, process) {
             return $.get(route, {
                 query: query
             }, function (data) {
                 var res = [];
-                for(d of data)
+                for (d of data)
                     res.push(d.title)
                 return process(res);
             });
