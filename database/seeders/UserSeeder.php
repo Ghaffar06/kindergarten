dcd2ce8834448a0776687bca7be145e7a92e1f4a
Ghaffar Daher <ghaffardaher92@gmail.com>
2022-12-29 15:24:09 +0300

    some data DONT MIGRATE

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        (new User)->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+            'role_id' => (new Role)->where('title', '=', 'admin')->first('id')['id'],
+            'active' => 1,
+        ]);
+    }
+}
